410c34e27977d02122e809e080862a60a09fd188
«Aleksandr» <AlexusB92@yandex.ru>
2022-09-08 17:53:02 +0300

    Add task_6 to determine the parity of a number

---
+++ Lesson_001/task_6/task_6.cs
@@ -0,0 +1,11 @@
+﻿int a = new Random().Next(-100, 100);
+Console.Write("a = ");
+Console.WriteLine(a);
+if (a%2==0)
+{
+    Console.Write("четное число");
+}
+else
+{
+    Console.Write("не четное число");  
+}
