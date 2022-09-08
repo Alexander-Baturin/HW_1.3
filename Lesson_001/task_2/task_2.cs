f48f7d0a9e4dd1b0461461fe615a9499a12edc7f
«Aleksandr» <AlexusB92@yandex.ru>
2022-09-08 16:28:48 +0300

    Correct task_2 designation of the max and min

--- Lesson_001/task_2/task_2.cs
+++ Lesson_001/task_2/task_2.cs
@@ -2,12 +2,19 @@
 Console.WriteLine(a);
 int b = new Random().Next(0, 10);
 Console.WriteLine(b);
-int max = a;
+int max = 0;
+int min = 0;
 if(a>b)
 {
+    Console.Write("max = ");
     Console.WriteLine(max = a);
+    Console.Write("min = ");
+    Console.WriteLine(min = b);
 }
 else
 {
+    Console.Write("max = ");
     Console.WriteLine(max = b);
+    Console.Write("min = ");
+    Console.WriteLine(min = a);
 }
