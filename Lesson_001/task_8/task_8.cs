6ec8017afd89bab80d7e12ce310a929581cebd16
«Aleksandr» <AlexusB92@yandex.ru>
2022-09-08 22:35:27 +0300

    Add task_8 highlighting all even numbers

---
+++ Lesson_001/task_8/task_8.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
+Console.Write("Введите N = ");
+int N = int.Parse(Console.ReadLine());
+int count = N-N;
+while (count<=N)
+{
+    if (count%2==0)
+    {
+        Console.Write(count+ "; " );
+        count = count+2;
+    }
+    else
+    {
+        count++;
+    }
+}
