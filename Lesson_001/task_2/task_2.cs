bb58223e065cbe5f0a930587aff11e4b0282726b
«Aleksandr» <AlexusB92@yandex.ru>
2022-09-08 16:23:07 +0300

    Add task_2 designation of the max and min

---
+++ Lesson_001/task_2/task_2.cs
@@ -0,0 +1,13 @@
+﻿int a = new Random().Next(0, 10);
+Console.WriteLine(a);
+int b = new Random().Next(0, 10);
+Console.WriteLine(b);
+int max = a;
+if(a>b)
+{
+    Console.WriteLine(max = a);
+}
+else
+{
+    Console.WriteLine(max = b);
+}
