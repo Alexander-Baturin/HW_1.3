58e1f942ccf46e40d264f087a2d671ddffb926b6
«Aleksandr» <AlexusB92@yandex.ru>
2022-10-04 22:27:47 +0300

    Correct task 029

--- Lesson_004/Task_029/Task_029.cs
+++ Lesson_004/Task_029/Task_029.cs
@@ -8,11 +8,11 @@ Console.Write("[");
 for (int i = 0; i < numbers.Length; i++)
  {
     numbers [i] = new Random().Next(0, 11);
-    Console.Write(" " + Method (i) + " ");
+    Console.Write(" " + Mass (i) + " ");
  }
 Console.Write("]");
 
-int Method (int a)
+int Mass (int a)
 {
     return numbers[a];
 }
