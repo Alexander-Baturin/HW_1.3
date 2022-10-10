034103c37a8903bd6ddb550692c52cb47930236f
«Aleksandr» <AlexusB92@yandex.ru>
2022-10-10 21:00:41 +0300

    Correct task 068

--- Lesson_009/Task_068/Task_068.cs
+++ Lesson_009/Task_068/Task_068.cs
@@ -7,15 +7,15 @@ Console.Clear();
 int m = InputNumbers("Введите m: ");
 int n = InputNumbers("Введите n: ");
 
-int functionAkkerman = Ack(m, n);
+int functionAkkerman = Akk(m, n);
 
 Console.Write($"m = {m}, n = {n} -> {functionAkkerman} ");
 
-int Ack(int m, int n)
+int Akk(int m, int n)
 {
   if (m == 0) return n + 1;
-  else if (n == 0) return Ack(m - 1, 1);
-  else return Ack(m - 1, Ack(m, n - 1));
+  else if (n == 0) return Akk(m - 1, 1);
+  else return Akk(m - 1, Akk(m, n - 1));
 }
 
 int InputNumbers(string input) 
